c1c8cf0eaab786b241b944e88fc06aa3ed5d2a3f
Salvador-a <salvador.sar614@outlook.com>
2024-03-20 13:13:14 -0600

    Finalizando los primeros comentarios

---
+++ views/templates/footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__contenido">
+            <h3 class="footer__logo">
+                TeleUrban
+            </h3>
+
+            <p class="footer__texto">
+            s un canal de comunicación en transporte público con información y entretenimiento en monitores y bocinas en diversas estados de la República Mexicana.
+            </p>
+        </div>
+        <nav class="menu-redes"> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/TeleUrbanTV">
+                <span class="menu-redes__ocultar">Facebook</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com/TELE_URBAN?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+                <span class="menu-redes__ocultar">Twitter</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.youtube.com/@TeleUrban">
+                <span class="menu-redes__ocultar">YouTube</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/teleurban/">
+                <span class="menu-redes__ocultar">Instagram</span>
+            </a> 
+        </nav>
+    </div>
+
+    <p class="footer__copyrigth">
+        TeleUrban
+        <span class="footer__copyrigth--regular">
+            - Todos los derechos reservados <?php echo date('Y'); ?>
+        </span>
+
+    </p>
+
+</footer>
